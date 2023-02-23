f4f097bd1d2724b5b67d12084df498cfe8ac4c68
FunkySphere <krauzowiczs42@gmail.com>
2023-02-23 12:55:12 +0100

    layout change and input binds

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-analytics',
-  templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+  template: `
+    <div class="content">
+      <div class="card bg-dark">
+        <app-analytics-toolbar class="card-header"></app-analytics-toolbar>
+        <app-analytics-main class="card-body"></app-analytics-main>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./analytics.component.scss'],
 })
-export class AnalyticsComponent {
-
-}
+export class AnalyticsComponent {}
