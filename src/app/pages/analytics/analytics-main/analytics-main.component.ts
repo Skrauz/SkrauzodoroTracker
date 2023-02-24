7957f7cdc994be5c6a001740a763ae90dfded0f7
FunkySphere <krauzowiczs42@gmail.com>
2023-02-24 14:45:53 +0100

    some progress on figuring out chartjs

--- src/app/pages/analytics/analytics-main/analytics-main.component.ts
+++ src/app/pages/analytics/analytics-main/analytics-main.component.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs';
 import { Timespan } from 'src/app/database/timespans/timespanModel';
 // import {default as Annotation} from 'chartjs-plugin-annotation';
 import { TimespansService } from 'src/app/database/timespans/timespans.service';
-import { AnalyticsService } from '../analytics.service';
 
 @Component({
   selector: 'app-analytics-main',
@@ -13,13 +12,15 @@ import { AnalyticsService } from '../analytics.service';
   styleUrls: ['./analytics-main.component.scss'],
 })
 export class AnalyticsMainComponent implements OnInit {
-  constructor(private timespansService: TimespansService, private analyticsService: AnalyticsService) {}
+  constructor(
+    private timespansService: TimespansService,
+  ) {}
 
   ngOnInit(): void {
     this.refreshTimespans();
   }
 
-  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+  @ViewChild(BaseChartDirective) chartMain?: BaseChartDirective;
 
   chartType: ChartType = 'line';
 
@@ -29,32 +30,57 @@ export class AnalyticsMainComponent implements OnInit {
     this.timespans$ = this.timespansService.getTimespans();
   }
 
-  public dummyData: ChartConfiguration['data'] = {
+  chartDateRange: string[] = ['a','b','c']
+
+
+  public chartData: ChartConfiguration['data'] = {
+    labels: this.chartDateRange,
     datasets: [
       {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Test Data',
         backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
+        borderColor: 'rgb(75, 192, 192)',
         pointBackgroundColor: 'rgba(148,159,177,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      } ]
-    }
+        fill: true,
+      },
+    ],
+  };
 
+  legendColor: string = 'rgba(255,255,255,0.8)';
   public chartOptions: ChartConfiguration['options'] = {
+    plugins: {
+      legend: {
+        labels: {
+          color: this.legendColor,
+        },
+      },
+    },
     elements: {
       line: {
-        tension: 0.5
-      }
+        tension: 0,
+      },
     },
     scales: {
-      y:
-        {
-          position: 'left',
+      y: {
+        ticks: {
+          color: this.legendColor,
+        },
+        position: 'left',
+        grace: '10%',
+
+        grid: {
+          color: 'rgba(255,255,255,0.3)',
+        },
+      },
+      x: {
+        ticks: {
+          color: this.legendColor
         }
-    }
-  }
+      },
+    },
+  };
 }
--- src/app/pages/analytics/analytics-toolbar/analytics-toolbar.component.ts
+++ src/app/pages/analytics/analytics-toolbar/analytics-toolbar.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { AnalyticsService } from '../analytics.service';
+import { AnalyticsMainComponent } from '../analytics-main/analytics-main.component';
 
 @Component({
   selector: 'app-analytics-toolbar',
   templateUrl: './analytics-toolbar.component.html',
-  styleUrls: ['./analytics-toolbar.component.scss']
+  styleUrls: ['./analytics-toolbar.component.scss'],
 })
 export class AnalyticsToolbarComponent {
-  constructor(public analyticsService: AnalyticsService) {
+  constructor(private analyticsMain: AnalyticsMainComponent) {}
 
+  taskName?: string;
+  projectName?: string;
+  startDate?: Date;
+  endDate?: Date;
+
+  applyLabelRange() {
+    if (this.startDate && this.endDate) {
+      this.analyticsMain.chartData.labels = ['g','h'];
+      this.analyticsMain.chartMain?.update();
+      return;
+    }
   }
 }
