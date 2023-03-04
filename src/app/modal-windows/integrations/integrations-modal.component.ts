95f361c4b6f83c7b1da755cbee6a563a81e24268
FunkySphere <krauzowiczs42@gmail.com>
2023-03-04 17:18:55 +0100

    integrations and some layout/styling fixes

--- src/app/modal-windows/integrations/integrations-modal.component.ts
+++ src/app/modal-windows/integrations/integrations-modal.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { TodoistApi } from '@doist/todoist-api-typescript';
 
 @Component({
   selector: 'app-integrations-modal',
   templateUrl: './integrations-modal.component.html',
-  styleUrls: ['./integrations-modal.component.scss', "./../../pages/shared/shared-inputs.scss", './../../pages/shared/shared-modal.scss']
+  styleUrls: [
+    './../../pages/shared/shared-inputs.scss',
+    './../../pages/shared/shared-modal.scss',
+    './integrations-modal.component.scss',
+  ],
 })
 export class IntegrationsModalComponent {
   constructor(public modalRef: MdbModalRef<IntegrationsModalComponent>) {}
+
+  todoistToken?: string;
+
+  todoistApi?: TodoistApi;
+
+  constructApi(todoistToken?: string) {
+    if (todoistToken) {
+      this.todoistApi = new TodoistApi(todoistToken);
+    } else {
+      alert('Insert your API Token');
+    }
+  }
+
+  getProjects() {
+    if(this.todoistApi) {
+      this.todoistApi
+      .getProjects()
+      .then((projects) => console.log(projects))
+      .catch((error) => console.log(error));
+    }
+  }
 }
